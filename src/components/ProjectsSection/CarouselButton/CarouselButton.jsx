94fbe61b1291100c34bbbeceaedb3447b765ff87
Leyson <134742017+leysonn@users.noreply.github.com>
2024-02-18 16:56:19 +0300

    Незначительные изменения и исправления кнопок в секции projects и компонента Form

--- src/components/ProjectsSection/CarouselButton/CarouselButton.jsx
+++ src/components/ProjectsSection/CarouselButton/CarouselButton.jsx
@@ -2,7 +2,7 @@ import './CarouselButton.scss';
 
 function CarouselButton({ icons, onClick, isActive }) {
     return (
-        <button className={`carousel-btn${isActive ? '-active' : ''}`} onClick={onClick} disabled={!isActive}>
+        <button className="carousel-btn" onClick={onClick} disabled={!isActive}>
             <img src={isActive ? icons.active : icons.default} alt="Button icon" className="carousel-btn-img" draggable={false} />
         </button>
     );
