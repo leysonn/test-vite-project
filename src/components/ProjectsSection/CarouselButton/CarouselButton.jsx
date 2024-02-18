39fc9b3a02ad49fa0ab15a998b92d7a3bf855e03
Leyson <134742017+leysonn@users.noreply.github.com>
2024-02-18 15:41:52 +0300

    Исправлены кнопки в секции projects

--- src/components/ProjectsSection/CarouselButton/CarouselButton.jsx
+++ src/components/ProjectsSection/CarouselButton/CarouselButton.jsx
@@ -2,8 +2,8 @@ import './CarouselButton.scss';
 
 function CarouselButton({ icons, onClick, isActive }) {
     return (
-        <button className={`carousel-btn${isActive ? '-active' : ''}`} disabled={!isActive}>
-            <img src={isActive ? icons.active : icons.default} alt="Button icon" onClick={onClick} draggable={false} />
+        <button className={`carousel-btn${isActive ? '-active' : ''}`} onClick={onClick} disabled={!isActive}>
+            <img src={isActive ? icons.active : icons.default} alt="Button icon" className="carousel-btn-img" draggable={false} />
         </button>
     );
 }
